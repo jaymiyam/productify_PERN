b2796152f1575f5de7e1085e1feabf7ddb22b22a
Jaymi Yam <jaymiyam@gmail.com>
2026-04-07 17:11:39 +0800

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'success' });
+});
+
+app.listen(3000, () => {
+  console.log('server listening');
+});
