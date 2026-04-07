b922132d828a23a6958a50762f037b8b2681036c
Jaymi Yam <jaymiyam@gmail.com>
2026-04-08 01:13:59 +0800

    drizzle and postgresql setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,27 @@
+import { ENV } from './config/env';
 import express from 'express';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
 
 const app = express();
 
+app.use(cors({ origin: ENV.FRONTEND_URL }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(clerkMiddleware());
+
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'success' });
+  res.json({
+    message:
+      'Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth',
+    endpoints: {
+      users: '/api/users',
+      products: '/api/products',
+      comments: 'api/comments',
+    },
+  });
 });
 
-app.listen(3000, () => {
-  console.log('server listening');
+app.listen(ENV.PORT, () => {
+  console.log(`server listening on port ${ENV.PORT}`);
 });
